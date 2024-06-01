307309da259bb4b6d6180b0da13ea1196257089b
rudrag1410 <rudragupta1410@gmail.com>
2024-06-01 17:06:31 +0530

    created new routes

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,10 @@ import { z, ZodError } from "zod";
 import sheets from "./sheetClient.mjs";
 import moment from "moment-timezone";
 
-const SHEET_ID = process.env.SHEET_ID;
+const CLINIC_LEADS_SHEET_ID = process.env.SHEET_ID;
+const IVF_LEADS_SHEET_ID = process.env.IVF_LEADS_SHEET_ID;
 
-if (!SHEET_ID) {
+if (!CLINIC_LEADS_SHEET_ID, !IVF_LEADS_SHEET_ID) {
   throw new Error("SHEET_ID is not defined in the environment variables");
 }
 
@@ -33,7 +34,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-app.post("/send-message", async (req, res) => {
+app.post("/clinic_leads", async (req, res) => {
   try {
     const body = contactFormSchema.parse(req.body);
     const rows = Object.values(body);
@@ -45,8 +46,8 @@ app.post("/send-message", async (req, res) => {
     rows.push(currentDate, currentTime);
 
     await sheets.spreadsheets.values.append({
-      spreadsheetId: SHEET_ID,
-      range: "Data!A2:E2",
+      spreadsheetId: CLINIC_LEADS_SHEET_ID,
+      range: "Clinic_leads!A2:E2",
       insertDataOption: "INSERT_ROWS",
       valueInputOption: "RAW",
       requestBody: {
@@ -60,7 +61,41 @@ app.post("/send-message", async (req, res) => {
     res.status(status).json({ error: error.message });
   }
 });
+app.get("/clinic_leads", (req, res) => {
+  res.json({ message: "Clinic Leads Api Is Working" });
+});
 
+
+app.post("/ivf-lead", async (req, res) => {
+  try {
+    const body = contactFormSchema.parse(req.body);
+    const rows = Object.values(body);
+
+    const now = moment().tz("Asia/Kolkata");
+    const currentDate = now.format("DD-MM-YYYY");
+    const currentTime = now.format("hh:mm A");
+
+    rows.push(currentDate, currentTime);
+
+    await sheets.spreadsheets.values.append({
+      spreadsheetId: IVF_SHEET_ID,
+      range: "SEO_LEADS!A2:E2",
+      insertDataOption: "INSERT_ROWS",
+      valueInputOption: "RAW",
+      requestBody: {
+        values: [rows],
+      },
+    });
+    res.json({ message: "Data added successfully" });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    const status = error instanceof ZodError ? 400 : 500;
+    res.status(status).json({ error: error.message });
+  }
+});
+app.get("/ivf-lead", (req, res) => {
+  res.json({ message: "IVF Center Leads Api Is Working" });
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
